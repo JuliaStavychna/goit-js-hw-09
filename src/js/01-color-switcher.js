54118f399e1ef21d69df431d620a3ef64f6b3d4a
Julia <129061308+JuliaStavychna@users.noreply.github.com>
2023-10-03 17:24:23 +0300

    updates

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,41 +1,36 @@
 const elements = {
-    startBtn: document.querySelector('[data-start]'),
-    stopBtn: document.querySelector('[data-stop]'),
-  };
-  
-  elements.startBtn.addEventListener('click', onStartBtnClick);
-  elements.stopBtn.addEventListener('click', onStopBtnClick);
-  
-  let timeId = null;
-  let startBtnActive = false;
-  
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+elements.startBtn.addEventListener('click', onStartBtnClick);
+elements.stopBtn.addEventListener('click', onStopBtnClick);
+
+let timeId = null;
+let startBtnActive = false;
+
+elements.stopBtn.disabled = true;
+
+function onStartBtnClick() {
+  timeId = setInterval(changeColor, 1000);
+  elements.stopBtn.disabled = false;
+
+  elements.startBtn.disabled = true;
+}
+
+function onStopBtnClick() {
+  clearInterval(timeId);
+
+  elements.startBtn.disabled = false;
   elements.stopBtn.disabled = true;
-  
-  function onStartBtnClick() {
-    timeId = setInterval(changeColor, 1000);
-    elements.stopBtn.disabled = false;
-  
-    if (!startBtnActive) {
-      elements.startBtn.disabled = true;
-      elements.startBtn.removeEventListener('click', onStartBtnClick);
-    }
-  }
-  
-  function onStopBtnClick() {
-    clearInterval(timeId);
-    if (!startBtnActive) {
-      elements.startBtn.disabled = false;
-      elements.startBtn.addEventListener('click', onStartBtnClick);
-      elements.stopBtn.disabled = true;
-    }
-  }
-  
-  function changeColor() {
-    document.body.style.backgroundColor = getRandomHexColor();
-  }
-  
-  function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
-  }
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
